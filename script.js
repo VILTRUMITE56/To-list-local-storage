d8db35a8d7ce85666f03d256be78271c4777d0e7
Gabriel Ybanez <ybagabrielfrancis@gmail.com>
2025-09-01 00:08:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,18 @@ taskForm.addEventListener("submit", (e) => {
 const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 
 const taskObj = {
-  id: `${titleInput.value.toLowerCase().split(" ").join("-")}`,
+  id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+  title: titleInput.value,
+  date: dateInput.value,
+  description: descriptionInput,
 };
 
-console.log(taskObj);
+if (dataArrIndex === -1) {
+  taskData.unshift(taskObj);
+}
+
+taskData.forEach(({ id, title, date, description }) => {
+  tasksContainer.innerHTML += `
+  <div class="task" id="${id}"></div>
+  `;
+});
