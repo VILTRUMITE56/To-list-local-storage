8febe0a54e7a331638909b76307d25d15b057387
Gabriel Ybanez <ybagabrielfrancis@gmail.com>
2025-09-06 23:36:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -30,14 +30,16 @@ const addOrUpdateTask = () => {
 };
 
 const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
+
   taskData.forEach(({ id, title, date, description }) => {
     tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
+          <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button> 
         </div>
       `;
   });
